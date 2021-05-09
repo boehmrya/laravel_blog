5615cb1788fb14e4f3c81aa8d31b303f6e771aad
Ryan <ryan.w.boehm@gmail.com>
2021-05-09 10:18:28 -0400

    created blade component

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,29 +1,13 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<x-layout>
+  <article>
+    <h1>
+      {{ $post->title }}
+    </h1>
 
-        <title>Posts</title>
+    <div>
+      {!! $post->body !!}
+    </div>
+  </article>
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-        <link rel="stylesheet" href="/app.css">
-    </head>
-
-    <body class="antialiased">
-      <main>
-        <article>
-          <h1>
-            {{ $post->title }}
-          </h1>
-
-          <div>
-            {!! $post->body !!}
-          </div>
-        </article>
-
-        <a href="/">Go Back</a>
-      </main>
-    </body>
-</html>
+  <a href="/">Go Back</a>
+</x-layout>
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,31 +1,15 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<x-layout>
+    @foreach ($posts as $post)
+      <article>
+        <h1>
+          <a href="/posts/{{ $post->slug }}">
+            {{ $post->title }}
+          </a>
+        </h1>
 
-        <title>Posts</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-        <link rel="stylesheet" href="/app.css">
-    </head>
-
-    <body class="antialiased">
-      <main>
-        @foreach ($posts as $post)
-          <article>
-            <h1>
-              <a href="/posts/{{ $post->slug }}">
-                {{ $post->title }}
-              </a>
-            </h1>
-
-            <div>
-              {{ $post->excerpt }}
-            </div>
-          </article>
-        @endforeach
-      </main>
-    </body>
-</html>
+        <div>
+          {{ $post->excerpt }}
+        </div>
+      </article>
+    @endforeach
+</x-layout>
