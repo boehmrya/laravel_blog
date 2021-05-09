84a9b1a983fe4a8ea4dc96f96a7d4672383d0479
Ryan <ryan.w.boehm@gmail.com>
2021-05-08 22:20:40 -0400

    set up handling of meta data

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -14,7 +14,8 @@
     <body class="antialiased">
       <main>
         <article>
-          <?= $post; ?>
+          <h1><?= $post->title; ?></h1>
+          <div><?= $post->body; ?></div>
         </article>
 
         <a href="/">Go Back</a>
