b349ee503efef35324a2733b00ceb66cb3a459ac
Ryan <ryan.w.boehm@gmail.com>
2021-05-11 21:07:43 -0400

    created new posts model

--- routes/web.php
+++ routes/web.php
@@ -20,9 +20,9 @@ Route::get('/', function () {
     ]);
 });
 
-Route::get('/posts/{post}', function ($slug) {
+Route::get('/posts/{post}', function ($id) {
   // find a post by slug and pass it to a view called "post"
   return view('post', [
-    'post' => Post::find($slug)
+    'post' => Post::find($id)
   ]);
 });
