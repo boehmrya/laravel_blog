4c101d03bfe06263a5dbf7fcbb13b5db71859d39
Ryan <ryan.w.boehm@gmail.com>
2021-06-06 14:21:40 -0500

    updated post and posts templates

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use App\Models\User;
+use App\Models\Post;
 use App\Models\Category;
 
 class DatabaseSeeder extends Seeder
@@ -17,6 +18,7 @@ class DatabaseSeeder extends Seeder
     {
 
         User::truncate();
+        Post::truncate();
         Category::truncate();
 
         $user = User::factory()->create();
@@ -41,8 +43,17 @@ class DatabaseSeeder extends Seeder
           'category_id' => $family->id,
           'title' => 'My Family Post',
           'slug' => 'my-first-post',
-          'excerpt' => 'Lorem ipsum dolar ist amet.',
-          'body' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi sed ante lectus. Aliquam in scelerisque nulla, vitae tristique eros. Sed tincidunt, elit at auctor cursus, risus odio rutrum nisi, nec facilisis enim velit in velit.'
-        ])
+          'excerpt' => '<p>Lorem ipsum dolar ist amet.</p>',
+          'body' => '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi sed ante lectus. Aliquam in scelerisque nulla, vitae tristique eros. Sed tincidunt, elit at auctor cursus, risus odio rutrum nisi, nec facilisis enim velit in velit.</p>'
+        ]);
+
+        Post::create([
+          'user_id' => $user->id,
+          'category_id' => $work->id,
+          'title' => 'My Work Post',
+          'slug' => 'my-work-post',
+          'excerpt' => '<p>Lorem ipsum dolar ist amet.</p>',
+          'body' => '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi sed ante lectus. Aliquam in scelerisque nulla, vitae tristique eros. Sed tincidunt, elit at auctor cursus, risus odio rutrum nisi, nec facilisis enim velit in velit.</p>'
+        ]);
     }
 }
