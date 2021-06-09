9428c842dc5f782649107ee87018452d1999e930
Ryan <ryan.w.boehm@gmail.com>
2021-06-09 08:06:26 -0500

    set up user factory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,44 +16,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-
-        User::truncate();
-        Post::truncate();
-        Category::truncate();
-
-        $user = User::factory()->create();
-
-        $personal = Category::create([
-          'name' => 'Personal',
-          'slug' => 'personal'
-        ]);
-
-        $family = Category::create([
-          'name' => 'Family',
-          'slug' => 'family'
-        ]);
-
-        $work = Category::create([
-          'name' => 'Work',
-          'slug' => 'work'
-        ]);
-
-        Post::create([
-          'user_id' => $user->id,
-          'category_id' => $family->id,
-          'title' => 'My Family Post',
-          'slug' => 'my-first-post',
-          'excerpt' => '<p>Lorem ipsum dolar ist amet.</p>',
-          'body' => '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi sed ante lectus. Aliquam in scelerisque nulla, vitae tristique eros. Sed tincidunt, elit at auctor cursus, risus odio rutrum nisi, nec facilisis enim velit in velit.</p>'
-        ]);
-
-        Post::create([
-          'user_id' => $user->id,
-          'category_id' => $work->id,
-          'title' => 'My Work Post',
-          'slug' => 'my-work-post',
-          'excerpt' => '<p>Lorem ipsum dolar ist amet.</p>',
-          'body' => '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi sed ante lectus. Aliquam in scelerisque nulla, vitae tristique eros. Sed tincidunt, elit at auctor cursus, risus odio rutrum nisi, nec facilisis enim velit in velit.</p>'
-        ]);
+        Post::factory(5)->create();
     }
 }
